3f100a2ee88727ad17f356b7eff4c38294ae1945
John Elliot V <jj5@jj5.net>
2025-12-24 16:17:09 +1100

    Work, work...

--- advent-2025-12-2-b.py
+++ advent-2025-12-2-b.py
@@ -6,8 +6,12 @@ from neopixel import NeoPixel
 # Define the strip pin number (28) and number of LEDs (15)
 strip = NeoPixel(Pin(28), 15)
 
+time.sleep(1)
+
 # iterate over 15 leds
 for i in range(15):
+    
+    print(i)
     
     # Set each LED in the range to yellow
     strip[i] = (255,0,0)
